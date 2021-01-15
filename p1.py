d774b2ceca3b1c12693f8e7c5dd1f02d1c19848b
soomyy <aaali@ualberta.ca>
2021-01-14 22:30:11 -0700

    modified script

--- p1.py
+++ p1.py
@@ -1,8 +1,15 @@
-
 import requests
 print(requests.__version__)
 
+# part 5
 content = requests.get("http://google.com")
 print(content)
 
+# part 7 - download file
+my_url = "https://raw.githubusercontent.com/asmao7/cmput404labs/main/p1.py"
+content = requests.get(my_url)
+print(content.text)
 
+filename = content.url[my_url.rfind('/')+1:]
+with open(filename, 'wb') as out_file :
+    out_file.write(content.content)
